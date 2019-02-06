6f214922cbf08adb13086771a88578305ea53b28
sarahcianfrone <scianfro@gmu.edu>
2019-02-06 15:23:49 -0500

    Added user input and functionality for snake

--- digital1/js/main.js
+++ digital1/js/main.js
@@ -7,15 +7,21 @@ window.onload = function() {
 	var HEIGHT=800;
 	
 	var SNAKE_SPACE = 2;
-	
+	var SNAKE_TILE = WIDTH/20;
+
 	var PONG_SPACE = 8;
 	var PONG_WIDTH = 20;
 	var PONG_HEIGHT = 100;
 
 
+	var up;
+	var down;
+	var left;
+	var right;
+
 	var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
-	var gameStarted = true;
+	var gameStarted = false;
 
 	var graphics;
 
@@ -27,29 +33,38 @@ window.onload = function() {
 	var paddle2;
 	var pongBall;
 
+	var snake;
+	var tailX=[10]
+	var tailY=[10]
+
 	function create() {
-	
 		graphics = game.add.graphics(0,0);
 
 		paddle1 = {xPos: PONG_SPACE, yPos: PONG_SPACE, xVel: 0, yVel: 0, accel: 10, maxVel: 20};
 		paddle2 = {xPos: WIDTH-(PONG_SPACE+PONG_WIDTH), yPos: PONG_SPACE, xVel: 0, yVel: 0, accel: 10, maxVel: 20};
-
 		pongBall = {xPos: (WIDTH-PONG_WIDTH)/2, yPos: (HEIGHT-PONG_WIDTH)/2, xVel:10, yVel:10};
-	}
 
+		snake = {xPos:10, yPos:10, xVel:0, yVel:0, score:0, tailLength:3, tailX=[10], tailY=[10], speed:10}
+		apple = {x:Math.floor(Math.random() * 20), y:Math.floor(Math.random()*20);}
+
+		up=game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		down=game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		left=game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		right=game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+	}
 	function update() {
 		drawPong();
 		movePong();
 		testCollision();
+		drawSnake();
+		moveSnake();
 	}
 
 	function drawPong(){
-		graphics.beginFill(0x000000, 1);
-		graphics.drawRect(0, 0, WIDTH, HEIGHT);
-		graphics.beginFill(0xFFFFFF, 1);
-		graphics.drawRect(paddle1.xPos, paddle1.yPos, PONG_WIDTH, PONG_HEIGHT);
-		graphics.drawRect(paddle2.xPos, paddle2.yPos, PONG_WIDTH, PONG_HEIGHT);
-		graphics.drawRect(pongBall.xPos, pongBall.yPos, PONG_WIDTH, PONG_WIDTH);
+		drawRect(0, 0, WIDTH, HEIGHT, 0x000000);
+		drawRect(paddle1.xPos, paddle1.yPos, PONG_WIDTH, PONG_HEIGHT, 0xFFFFFF);
+		drawRect(paddle2.xPos, paddle2.yPos, PONG_WIDTH, PONG_HEIGHT,0xFFFFFF);
+		drawRect(pongBall.xPos, pongBall.yPos, PONG_WIDTH, PONG_WIDTH, 0xFFFFFF);
 	}
 
 	function movePong(){
@@ -80,6 +95,62 @@ window.onload = function() {
 		}
 		if(pongBall.yPos >= HEIGHT-PONG_WIDTH)	pongBall.yVel *= -1;
 		if(pongBall.yPos <= 0) pongBall.yVel *= -1;
+	}
+
+	function drawSnake(){
+		for(var i;i<snake.tailLength;i++){
+			drawRect(snake.tailX[i]*SNAKE_TILE+SNAKE_SPACE, snake.tailY[i]*SNAKE_TILE+SNAKE_SPACE, SNAKE_TILE-(2*SNAKE_SPACE), SNAKE_TILE-(2*SNAKE_SPACE), 0x0000FF);
+		}
+		drawRect(apple.x*SNAKE_TILE, apple.y*SNAKE_TILE, SNAKE_TILE, SNAKE_TILE, 0xFF0000);
+		drawRect(snake.xPos*SNAKE_TILE+SNAKE_SPACE, snake.yPos*SNAKE_TUKE+SNAKE_SPACE, SNAKE_TILE-(2*SNAKE_SPACE), SNAKE_TILE-(2*SNAKE_SPACE), 0x00FF00);
+
+	}
+
+	function moveSnake(){
+		if(snake.xPos == apple.x && snake.yPos == apple.y){
+			snake.tailSize++;
+			//score increase
+			apple.x = Math.floor(Math.random() * 20);
+			apple.y = Math.floor(Math.random() * 20);
+		}
+		snake.tailX.unshift(snake.xPos);
+		snake.tailY.unshift(snake.yPos);
+		if(snake.tailX.length > snake.tailLength){
+			snake.tailX.pop();
+			snake.tailY.pop();
+		}
+		snake.xPos += snake.xVel;
+		snake.yPos += snake.yVel;
+
+		if(snake.xPos >= WIDTH/SNAKE_TILE) snake.xPos = 0;
+		if(snake.xPos < 0) snake.xPos = WIDTH/SNAKE_TILE-1;
+		if(snake.yPos >= HEIGHT.SNAKE_TILE) snake.yPos = 0;
+		if(snake.yPos < 0) snake.yPos = HEIGHT/SNAKE_TILE-1;
+	}
+
+	function checkKeys(){
+		if(left.isDown){
+			snake.xVel=-1*snake.speed;
+			snake.yVel=0;
+			gameStarted=true;
+		} else if(right.isDown){
+			gameStarted=true;
+			snake.xVel=snake.speed;
+			snake.yVel=0;
+		} else if(up.isDown){
+			gameStarted=true;
+			snake.xVel=0;
+			snake.yVel=-1*snake.speed;
+		} else if(down.isDown){
+			gameStarted=true;
+			snake.xVel=0;
+			snake.yVel=snake.speed;
+		} 
+	}
+
+	function drawRect(x, y, w, h, c){
+		graphics.beginFill(c, 1);
+		graphics.drawRect(x, y, w, h);
 	}
 	
 };
