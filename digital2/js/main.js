12c160921d4f981d80873596a4e346266c6d74fc
sarahcianfrone <scianfro@gmu.edu>
2019-02-13 14:25:17 -0500

    Started digital game 2!

---
+++ digital2/js/main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+window.onload = function(){
+
+	var WIDTH = 800;
+	var HEIGHT = 800;
+
+	var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
+
+
+	function preload() {
+		game.load.image('p1solid', 'assets/purplesolid.png');
+		game.load.image('p2solid', 'assets/greensolid.png');
+		game.load.image('p1pattern', 'assets/purplepattern.png');
+		game.load.image('p2pattern', 'assets/greenpattern.png');
+		game.load.image('ground', 'assets/Ground.png');
+		game.load.image('background', 'assets/background.png');
+	}
+	
+	var p1Solid;
+	var p1Pattern;
+	var p2Solid;
+	var p2Pattern;
+
+	var platforms;
+
+	function create() {
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.physics.arcade.gravity.y = 100;
+		game.add.sprite(0, 0, 'background');
+
+
+		platforms = game.add.group();
+		platforms.enableBody = true;
+		var plat = platforms.create(0, HEIGHT-50, 'ground');
+		plat.body.immovable = true;
+		
+		plat = platforms.create(0, HEIGHT-220, 'ground');
+		plat.scale.setTo(0.25 , 0.4);
+		plat.body.immovable = true;
+		
+		plat = platforms.create(WIDTH-200, HEIGHT-220, 'ground');
+		plat.scale.setTo(0.25, 0.4);
+		plat.body.immovable = true;
+		
+		plat = platforms.create(250, HEIGHT-390, 'ground');
+		plat.scale.setTo(0.625, 0.4);
+		plat.body.immovable = true;
+
+		plat = platforms.create(20, 100, 'ground');
+		plat.scale.setTo(0.3, 0.4);
+		plat.body.immovable = true;
+
+		plat = platforms.create(WIDTH-260, 100, 'ground');
+		plat.scale.setTo(0.3, 0.4);
+		plat.body.immovable = true;
+
+
+		p1Solid = game.add.sprite(100, 100, 'p1solid');
+		p1Pattern = game.add.sprite(100, 300, 'p1pattern');
+		p2Solid = game.add.sprite(300, 100, 'p2solid');
+		p2Pattern = game.add.sprite(300, 300, 'p2pattern');
+
+	
+
+	}
+	function update() {
+
+	}
+
+}
