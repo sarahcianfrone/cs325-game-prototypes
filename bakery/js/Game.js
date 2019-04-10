255ec2ee607e31ad050e8581e0b093820281354d
sarahcianfrone <scianfro@gmu.edu>
2019-04-10 14:33:31 -0400

    Added basic button stuff

--- bakery/js/Game.js
+++ bakery/js/Game.js
@@ -4,6 +4,8 @@ GameStates.makeGame = function( game, shared ) {
     //Constant fields
     const WIDTH = 800;
     const HEIGHT = 800;
+    //Multiplier for each consecutive buy
+    const INCREASE = 1.2;
 
     const WHITE_BREAD = 0;
     const WHEAT_BREAD = 1;
@@ -22,6 +24,7 @@ GameStates.makeGame = function( game, shared ) {
     var frame = 0;
 
     var units = [];
+    var buttons = [];
 
     function decreaseTimeLeft(){
         frame = 0;
@@ -94,6 +97,40 @@ GameStates.makeGame = function( game, shared ) {
         }
     }
 
+    function Button(x, y, ind, clickFunc){
+        var styleL = {font: "30px Arial", fill: "#fff", boundsAlignH: "left"};
+        var styleR = {font: "20px Arial", fill: "#fff", boundsAlignH: "right"};
+        var button = {  sprite: game.add.sprite(x, y, 'buttonBase'),
+                        nameText: game.add.text(x+5, y+5, units[ind].name, styleL),
+                        perSecText: game.add.text(x+150, y+5, "$/s = "+units[ind].perSec, styleR),
+                        numOwnedText: game.add.text(x+150, y+35, "Owned : "+units[ind].numOwned, styleR),
+                        costText: game.add.text(x+150, y+65, "Cost = "+units[ind].cost, styleR), 
+                        index: ind,
+                        func: clickFunc
+                    };
+         
+    }
+
+    function makeButtons(){
+        Button(20, 50, WHITE_BREAD, function(){
+            if(money >= units[WHITE_BREAD].cost){
+                money-=units[WHITE_BREAD].cost;
+                units[WHITE_BREAD].cost*=INCREASE;
+                units[WHITE_BREAD].numOwned++;
+            }
+        });
+    }
+
+    function destroyButtons(){
+        for(var i=0;i<buttons.length;i++){
+            buttons[i].sprite.destroy();
+            buttons[i].nameText.destroy();
+            buttons[i].perSecText.destroy();
+            buttons[i].numOwnedText.destroy();
+            buttons[i].costText.destroy();
+        }
+    }
+
     function displayMoney(){
         var floorMoney = Math.floor(money);
         moneyText.setText(numberToString(floorMoney));
@@ -107,6 +144,8 @@ GameStates.makeGame = function( game, shared ) {
         enemyMoneyText.destroy();
         moneyText.destroy();
         background.destroy();
+
+        destroyButtons();
         //  Then let's go back to the main menu.
         game.state.start('MainMenu');
 
@@ -118,9 +157,9 @@ GameStates.makeGame = function( game, shared ) {
             background = game.add.sprite(0, 0, 'background');
             timeLeftText = game.add.text(WIDTH - 50, 10, "5:00", {font: "25px Arial", fill: "#000", boundsAlignH: "right"});
             enemyMoneyText = game.add.text(WIDTH - 100, 50, ""+enemyMoney, {font: "25px Arial", fill: "#000", boundsAlignH: "right"});
-            moneyText = game.add.text(WIDTH/2, 100, "0", {font: "40px Arial", fill: "#000", boundsAlignH: "right"});
+            moneyText = game.add.text(WIDTH/2, 100, "0", {font: "40px Arial", fill: "#000", boundsAlignH: "center"});
             key = game.input.keyboard.addKey(Phaser.Keyboard.W);
-            
+            makeButtons();
             makeUnits();
         },
     
@@ -130,7 +169,6 @@ GameStates.makeGame = function( game, shared ) {
             if(timeLeft > 0) enemyMoneyIncrease();
             moneyIncrease();
             displayMoney();
-            if (key.isDown) units[BRIOCHE].numOwned++;
         }
     };
 };
